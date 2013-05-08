f902de860c9b17c4d5ba358222073909a766e853
Tero Mäntylä <termanty@cs.helsinki.fi>
2013-05-08 22:20:17 +0300

    IdaStar luoka lisätty

---
+++ FifteenPuzzle/src/fifteenpuzzle/IdaStar.java
@@ -0,0 +1,20 @@
+package fifteenpuzzle;
+
+/**
+ * IdaStar Class
+ * Class uses IDA* algorith to find the least amount of movements to solve a puzzle.
+ * 
+ * @author Tero Mäntylä
+ */
+
+public class IdaStar {
+    
+    public String findPath() {
+        return null;
+    }
+    
+    public void evalue(){   
+    }
+    
+    
+}
