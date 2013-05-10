46b34609d26010fa6b98c4cb0cbc5f127b2b0047
Tero Mäntylä <termanty@cs.helsinki.fi>
2013-05-10 22:46:28 +0300

    Brute luokan lisäys

---
+++ FifteenPuzzle/src/fifteenpuzzle/BruteForce.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fifteenpuzzle;
+
+/**
+ *
+ * @author Tero Mäntylä
+ */
+public class BruteForce {
+    
+    private byte[][] p;
+   
+
+    public BruteForce(byte[][] p) {
+        this.p = p;
+    }
+    
+    public String solve() {
+        
+        
+        
+        return null;
+    }    
+    
+    
+}
